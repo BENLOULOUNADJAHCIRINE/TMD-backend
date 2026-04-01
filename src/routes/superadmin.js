4d93b3f4e3b0906784a1864d6be7614581b0544c
Ikram <khelilikram315@gmail.com>
2026-04-01 23:06:52 +0100

    add the superadmin controller and route

--- src/routes/superadmin.js
+++ src/routes/superadmin.js
@@ -1,3 +1,11 @@
+
 const express = require('express');
 const router = express.Router();
-module.exports = router;
+const { getAdmins, createAdmin, deleteAdmin } = require('../controllers/superadminController');
+const { protect, isSuperAdmin } = require('../middleware/auth');
+
+router.get('/admins', protect, isSuperAdmin, getAdmins);
+router.post('/admins', protect, isSuperAdmin, createAdmin);
+router.delete('/admins/:id', protect, isSuperAdmin, deleteAdmin);
+
+module.exports = router;  
